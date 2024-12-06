68cd4d284054639425d76651c58649b1a943344a
martinfiguerola <fedefiguerola@gmail.com>
2024-12-06 10:21:23 +0100

    create-entities-relationships

--- src/main/java/com/martin/veterinary_clinic_api/entities/Owner.java
+++ src/main/java/com/martin/veterinary_clinic_api/entities/Owner.java
@@ -1,6 +1,34 @@
 package com.martin.veterinary_clinic_api.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "owners")
+@Getter @Setter
 public class Owner {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String lastname;
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+    @OneToOne(mappedBy = "owner")
+    private Pet pet;
 
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", pet=" + pet +
+                '}';
+    }
 }
--- src/main/java/com/martin/veterinary_clinic_api/entities/Pet.java
+++ src/main/java/com/martin/veterinary_clinic_api/entities/Pet.java
@@ -1,6 +1,38 @@
 package com.martin.veterinary_clinic_api.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pets")
+@Getter @Setter
 public class Pet {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String species;
+    @Column
+    private String breed;
+    @Column
+    private String color;
+    @OneToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", species='" + species + '\'' +
+                ", breed='" + breed + '\'' +
+                ", color='" + color + '\'' +
+                ", owner=" + owner +
+                '}';
+    }
 }
